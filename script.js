1ffd446f081510bcc6f3cec4eb7c96b5b84fa049
lemonade1717 <gaastreet@gmail.com>
2024-08-23 16:12:22 +0200

    scriptBaseAdded

---
+++ script.js
@@ -0,0 +1,22 @@
+const movieForm = document.querySelector(".movieForm");
+const movieInput = document.querySelector(".movieInput");
+const card = document.querySelector(".card");
+const movieDiv = document.querySelector(".movie");
+const movieInfo = document.querySelector(".movieInfo");
+const apiKey = "ffcde258";
+
+movieForm.addEventListener("submit", async event => {
+
+});
+
+async function getMovieData(name) {
+
+}
+
+function displayMovieInfo(data) {
+
+}
+
+function displayError(message) {
+
+}
